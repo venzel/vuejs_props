42a257b477c077b8d1d540d690bb0b88c27af9dd
venzel <eneas.eng@yahoo.com>
2022-02-14 10:52:27 -0300

    feat: setup inicial

---
+++ src/components/UsuarioInfo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Name: {{ p_name }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['p_name'],
+};
+</script>
+
+<style scoped>
+.componente {
+    flex: 1;
+    background-color: #ec485f;
+    color: #fff;
+}
+</style>
